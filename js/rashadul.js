9c2f5d2a7c9ed321f288c64c1eed4eb530c65142
rashadulalam <rashadul91@gmail.com>
2023-01-23 20:59:09 +0600

    Some fixation

--- js/rashadul.js
+++ js/rashadul.js
@@ -40,3 +40,19 @@ cForm.addEventListener("submit", function( e ) {
     }
 
 });
+
+
+
+/**
+ * Pre-loader
+ */
+
+window.onload = function () {
+    const preloaderArea = document.getElementById("preloader-area");
+
+    preloaderArea.className = "page-loaded";
+
+    setTimeout(function() {
+        preloaderArea.style.display = "none";
+    }, 500);
+}
